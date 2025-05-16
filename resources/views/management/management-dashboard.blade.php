3a376c6a329fb0eea16dd2e7a3b3111325256b15
Exalted02 <exaltedsol02@gmail.com>
2025-05-16 15:28:04 +0530

    up

--- resources/views/management/management-dashboard.blade.php
+++ resources/views/management/management-dashboard.blade.php
@@ -3,32 +3,100 @@
     <div class="container">
 		<h2 class="page-title">Welcome to your overview</h2>
 		<div class="page-subtitle">Check out how your factory is performing</div>
-		<div class="pt-3 pb-3">
-			<div class="row">
-				<div class="col-md-4 col-sm-4 col-xs-4">
+		<div class="pt-2 pb-2">
+			<div class="row ">
+				<div class="col-md-4 col-sm-4 col-xs-4 small-card-first">
 					<div class="bg small-card">
-						<div class="bg small-card-title">No. of inspections</div>
-						<div class="bg small-card-counter">42</div>
-						<div class="bg small-card-counter-title">WEEKLY</div>
+						<div class="small-card-title">No. of inspections</div>
+						<div class="small-card-counter">42</div>
+						<div class="small-card-counter-title">WEEKLY</div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-4 col-xs-4">
+				<div class="col-md-4 col-sm-4 col-xs-4 small-card-second">
 					<div class="bg small-card">
-						<div class="bg small-card-title">No. of observations</div>
-						<div class="bg small-card-counter">8</div>
-						<div class="bg small-card-counter-title">WEEKLY</div>
+						<div class="small-card-title">No. of observations</div>
+						<div class="small-card-counter">8</div>
+						<div class="small-card-counter-title">WEEKLY</div>
 					</div>
 				</div>
-				<div class="col-md-4 col-sm-4 col-xs-4">
+				<div class="col-md-4 col-sm-4 col-xs-4 small-card-third">
 					<div class="bg small-card">
-						<div class="bg small-card-title">Time to close observation</div>
-						<div class="bg small-card-counter">2</div>
-						<div class="bg small-card-counter-title">DAYS</div>
+						<div class="small-card-title">Time to close observation</div>
+						<div class="small-card-counter">2</div>
+						<div class="small-card-counter-title">DAYS</div>
 					</div>
 				</div>
 			</div>
 		</div>
     </div>
+	<div class="management-location-card pt-2 pb-2">
+		<div class="container">
+			<div class="d-flex align-items-center location-header mb-3">
+				<img src="{{url('front-assets/static-image/5.jpg')}}" alt="Location">
+				<div>
+					<div class="title">Mandai Hill</div>
+					<small class="text-muted">Mandai Road 23, 532012</small>
+				</div>
+			</div>
+			<div class="row ">
+				<div class="col-md-4 col-sm-4 col-xs-4 small-card-first">
+					<div class="small-card">
+						<div class="small-card-title">No. of observations</div>
+						<div class="small-card-counter">4</div>
+						<div class="small-card-counter-title">WEEKLY</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 col-xs-4 small-card-second">
+					<div class="small-card">
+						<div class="small-card-title">Repeat observations</div>
+						<div class="small-card-counter">8</div>
+						<div class="small-card-counter-title">WEEKLY</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 col-xs-4 small-card-third">
+					<div class="small-card">
+						<div class="small-card-title">Time to close observations</div>
+						<div class="small-card-counter">6</div>
+						<div class="small-card-counter-title">DAYS</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="management-location-card pt-2 pb-2">
+		<div class="container">
+			<div class="d-flex align-items-center location-header mb-3">
+				<img src="{{url('front-assets/static-image/4.jpg')}}" alt="Location">
+				<div>
+					<div class="title">Fernavale</div>
+					<small class="text-muted">Mandai Road 23, 532012</small>
+				</div>
+			</div>
+			<div class="row ">
+				<div class="col-md-4 col-sm-4 col-xs-4 small-card-first">
+					<div class="small-card">
+						<div class="small-card-title">No. of observations</div>
+						<div class="small-card-counter">4</div>
+						<div class="small-card-counter-title">WEEKLY</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 col-xs-4 small-card-second">
+					<div class="small-card">
+						<div class="small-card-title">Repeat observations</div>
+						<div class="small-card-counter">8</div>
+						<div class="small-card-counter-title">WEEKLY</div>
+					</div>
+				</div>
+				<div class="col-md-4 col-sm-4 col-xs-4 small-card-third">
+					<div class="small-card">
+						<div class="small-card-title">Time to close observations</div>
+						<div class="small-card-counter">6</div>
+						<div class="small-card-counter-title">DAYS</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection 
 @section('scripts')
 
