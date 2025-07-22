e28de326bbf9d4272f4aa6af63f884ac1794d6b1
exaltedsol04 <exaltedsol04@gmail.com>
2025-07-22 15:23:37 +0530

    comment

---
+++ app/Models/Task_list_corrective_action_details.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task_list_corrective_action_details extends Model
+{
+    use HasFactory;
+	protected $fillable = [
+        'task_list_corrective_action_id', 
+        'lo_corrective_action_plan_final_checks', 
+        'ia_los_rejected_reason', 
+		'inspector_action_date',
+		'los_action_date', 
+        'approved_status', 
+		'rejected_status',
+		'rejected_repeated',
+    ];
+}
