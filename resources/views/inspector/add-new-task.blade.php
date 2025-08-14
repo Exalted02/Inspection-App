102f098184268a13744e8153962bca68936b345a
exaltedsol04 <exaltedsol04@gmail.com>
2025-08-14 19:56:35 +0530

    comment

--- resources/views/inspector/add-new-task.blade.php
+++ resources/views/inspector/add-new-task.blade.php
@@ -150,6 +150,7 @@ $(document).ready(function() {
 	flatpickr("#set_time", {
     enableTime: false,
     dateFormat: "d M Y H:i",
+	minDate: "today",
 	//allowInput: true,
     onChange: function(selectedDates, dateStr, instance) {
 			if (selectedDates.length == 1) {
