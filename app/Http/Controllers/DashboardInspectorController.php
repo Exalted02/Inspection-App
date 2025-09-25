0b39b03adc8575291b7c82b369c878a41c7b3b47
exaltedsol04 <exaltedsol04@gmail.com>
2025-09-25 15:24:20 +0530

    comment

--- app/Http/Controllers/DashboardInspectorController.php
+++ app/Http/Controllers/DashboardInspectorController.php
@@ -2853,7 +2853,9 @@ class DashboardInspectorController extends Controller
 		
 		$excludedChecklistPairs = Task_list_corrective_action::whereNotIn('rejected_repeated', [0])
 			->whereIn('task_list_id', $taskListIds)
-			->where('lo_id', auth()->user()->id)  // add new 24-09-2025
+			->where('lo_id', auth()->user()->id) //add new 24-09-2025
+			->orWhere('lo_direct_approve', 0)
+			->orWhere('lo_direct_approve', 1)
 			->where(function ($q) {
 				$q->where(function ($q) {
 					$q->where('inspector_action', 0)->where('los_action', 0);
@@ -2889,7 +2891,9 @@ class DashboardInspectorController extends Controller
 		
 		$excludedSubChecklistPairs = Task_list_corrective_action::whereNotIn('rejected_repeated', [0])
 			->whereIn('task_list_id', $taskListIds)
-			//->where('lo_id', auth()->user()->id) // add new 24-09-2025
+			->where('los_id', auth()->user()->id) // add new 24-09-2025
+			->orWhere('lo_direct_approve', 0)
+			->orWhere('lo_direct_approve', 1)
 			->where(function ($q) {
 				$q->where(function ($q) {
 					$q->where('inspector_action', 0)->where('los_action', 0);
@@ -4180,6 +4184,8 @@ class DashboardInspectorController extends Controller
 		$excludedChecklistPairs = Task_list_corrective_action::whereNotIn('rejected_repeated', [0])
 			->whereIn('task_list_id', $taskListIds)
 			->where('lo_id', auth()->user()->id)  // add new 24-09-2025
+			->orWhere('lo_direct_approve', 0)
+			->orWhere('lo_direct_approve', 1)
 			->where(function ($q) {
 				$q->where(function ($q) {
 					$q->where('inspector_action', 0)->where('los_action', 0);
@@ -4216,6 +4222,8 @@ class DashboardInspectorController extends Controller
 		$excludedSubChecklistPairs = Task_list_corrective_action::whereNotIn('rejected_repeated', [0])
 			->whereIn('task_list_id', $taskListIds)
 			->where('lo_id', auth()->user()->id) // add new 24-09-2025
+			->orWhere('lo_direct_approve', 0)
+			->orWhere('lo_direct_approve', 1)
 			->where(function ($q) {
 				$q->where(function ($q) {
 					$q->where('inspector_action', 0)->where('los_action', 0);
