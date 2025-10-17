d4ff064a880d48ede3af1e398da5ab8529bb1836
exaltedsol04 <exaltedsol04@gmail.com>
2025-10-17 20:11:30 +0530

    comment

--- resources/views/inspector/category-subcategory.blade.php
+++ resources/views/inspector/category-subcategory.blade.php
@@ -116,7 +116,19 @@ foreach($approvedCompleted as $result)
 							@if($categoryData->isNotEmpty())
 								@foreach($categoryData as $categories)
 								@php 
-									$tot_checklist = App\Models\Checklist::where('status','!=', 2)->where('category_id', $categories->id)->count();
+								    
+									$task_type_data = App\Models\Task_lists::where('id', $task_id)->first();
+									if($task_type_data->task_type == 1)
+									{
+										$res = App\models\Task_categories_checklist_subchecklist::where('category_id', $categories->id)->where('task_list_id', $task_id)->orderBy('id', 'ASC')->get();
+										$tot_checklist = $res->pluck('checklist_id')->unique()->count();
+									}
+									else
+									{
+										$tot_checklist = App\Models\Checklist::where('status','!=', 2)->where('category_id', $categories->id)->count();
+									}
+									
+									
 									$tot_checklist_completed = App\Models\Task_list_checklists::where('task_list_id',$task_id)->where('category_id', $categories->id)->count();
 									$tot_subchecklist_completed = App\Models\Task_list_subchecklists::where('task_list_id', $task_id)->where('category_id', $categories->id)->distinct('task_list_checklist_id')->count();
 									$tot_completed_task = $tot_checklist_completed+$tot_subchecklist_completed;
